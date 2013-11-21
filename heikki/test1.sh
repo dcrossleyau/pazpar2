e0f2463e427ca3f445b92636bb90c36792e85a50
Heikki Levanto <heikki@indexdata.dk>
2013-11-21 14:15:32 +0100

    Self-sufficient tests, all configs included

--- heikki/test1.sh
+++ heikki/test1.sh
@@ -5,24 +5,30 @@
 #
 # (uses curl and xml-twig-tools)
 
-DIR=`cd ..; pwd`
-HDIR=$DIR/heikki
-CFG="$DIR/etc/heikki-test1.cfg"
+if [ "$1" == "clean" ]
+then
+  echo "Cleaning up"
+  rm -f $PIDFILE $YAZPIDFILE *.out *.log *~ 
+  exit
+fi
 
-PZ="$DIR/src/pazpar2"
+CFG="test1.cfg"
 
-PIDFILE=$HDIR/pz2.pid
-YAZPIDFILE=$HDIR/yaz-ztest.pid
+PZ="../src/pazpar2"
+
+PIDFILE=pz2.pid
+YAZPIDFILE=yaz-ztest.pid
 
 yaz-ztest -p $YAZPIDFILE -l yaz-ztest.log &
 
+rm -f *.out *.log
 
-$PZ -f $CFG  -w "$DIR/etc" -l $HDIR/pz2.log -p $PIDFILE &
+$PZ -f $CFG  -l pz2.log -p $PIDFILE &
 sleep 0.2 # make sure it has time to start
 echo "Init"
 curl -s "http://localhost:9017/?command=init" > init.out
 SESSION=`xml_grep --text_only "//session" init.out `
-cat init.out
+# cat init.out; echo
 echo "Got session $SESSION"
 SES="&session=$SESSION"
 
@@ -33,18 +39,17 @@ QRY="query=computer"
 SEARCH="command=search$SES&$QRY&sort=relevance"
 echo $SEARCH
 curl -s "http://localhost:9017/?$SEARCH" > search.out
-cat search.out
+cat search.out | grep search
 echo
 
 SHOW="command=show$SES&sort=relevance"
 echo $SHOW
 curl -s "http://localhost:9017/?$SHOW" > show.out
-echo "md-score:"
-grep "md-score" show.out
-echo "relevance:"
-grep "relevance" show.out
+grep "relevance" show.out | grep += | grep -v "(0)"
 echo
 
 echo "All done"
 kill `cat $PIDFILE`
 kill `cat $YAZPIDFILE`
+rm -f $PIDFILE $YAZPIDFILE
+
